56d462c76116399e666366e0c924ebbc61f7d09c
Anantpandey07 <anantpandey175@gmail.com>
2024-07-31 00:13:23 +0530

    changes After user login

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,10 +1,13 @@
 
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet'
-import { Menu } from 'lucide-react'
+import { CircleUserRound, Menu } from 'lucide-react'
 import { Separator } from './ui/separator'
 import { Button } from './ui/button'
+import { useAuth0 } from '@auth0/auth0-react';
+import MobileNavLinks from './MobileNavLinks';
 
 export default function MobileNav() {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   return (
     <Sheet>
         <SheetTrigger>
@@ -12,11 +15,13 @@ export default function MobileNav() {
         </SheetTrigger>
         <SheetContent className='space-y-3'>
             <SheetTitle>
-                <span>Welcome to QuickFoods.com!</span>
+              {isAuthenticated ? <span className='flex items-center font-bold gap-2'><CircleUserRound className='text-orange-500'/>
+              {user?.email}</span> : <span>Welcome to QuickFoods.com!</span>}
             </SheetTitle>
             <Separator/>
-            <SheetDescription className='flex'>
-            <Button className='flex-1 font-bold bg-orange-500 py-2'>LogIn</Button>
+            <SheetDescription className='flex flex-col gap-4'>
+              {isAuthenticated  ? <MobileNavLinks/> : <Button onClick={() => loginWithRedirect()} className='flex-1 font-bold bg-orange-500 py-2' >LogIn</Button>}
+            
             </SheetDescription>
         </SheetContent>
     </Sheet>
