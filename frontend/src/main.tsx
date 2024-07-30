45f9a5c3657220068a7c5a091527a1d9c767841d
Anantpandey07 <anantpandey175@gmail.com>
2024-07-30 22:46:30 +0530

    Login using AUTH0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import './global.css'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
+import Auth0providerwithNavigate from './auth/Auth0providerwithNavigate'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <Auth0providerwithNavigate>
       <AppRoutes/>
+      </Auth0providerwithNavigate>
     </Router>
   </React.StrictMode>,
 )
