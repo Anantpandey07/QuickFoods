132d627f66e245a7d4d8bd83566a3c2574bae458
Anantpandey07 <anantpandey175@gmail.com>
2024-07-31 00:28:03 +0530

    User Model

---
+++ Backend/src/models/user.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    auth0Id: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+    },
+    RoomNo: {
+        type: String,
+    },
+    Hostel: {
+        type: String,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
