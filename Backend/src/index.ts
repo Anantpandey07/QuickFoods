c89a0f31abb44e25d4b4a871b40ae68a8f339c5f
Anantpandey07 <anantpandey175@gmail.com>
2024-07-31 16:27:09 +0530

    create User Api

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import MyUserRoutes from "./routes/MyUserRoutes";
 
 mongoose.connect(process.env.MONGO as string).then(() => 
 console.log("connected to database"));
@@ -10,9 +11,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.get("/test", async (req: Request, res: Response) =>{
-    res.json({message : "Hello"})
-});
+app.use("/api/my/user", MyUserRoutes);
 
 app.listen(7000, ()=>{
     console.log("server started on localhost:7000");
