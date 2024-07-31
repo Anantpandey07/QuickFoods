a187dd9167a69aed499dd6542ffda334bfcfa54e
Anantpandey07 <anantpandey175@gmail.com>
2024-07-31 17:26:15 +0530

    Create User Request

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,24 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import Auth0providerwithNavigate from './auth/Auth0providerwithNavigate'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <QueryClientProvider client={queryClient}>
       <Auth0providerwithNavigate>
       <AppRoutes/>
       </Auth0providerwithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
