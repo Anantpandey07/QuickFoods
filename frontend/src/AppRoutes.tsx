7565bea1d604dc2b05f5315f6bd977952b4688d4
Anantpandey07 <anantpandey175@gmail.com>
2024-08-10 15:45:03 +0530

    Protect User Profile Route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,13 +3,21 @@ import Layout from "./layouts/layouts";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 const AppRoutes = () =>{
     return(
         <Routes>
             <Route path="/" element={<Layout showHero={true}><HomePage /></Layout>}/>
+
             <Route path="/auth-callback" element={<AuthCallbackPage/>}></Route>
+
+            <Route element={<ProtectedRoute/>}>
             <Route path="/user-profile" element={<Layout > <UserProfilePage/></Layout>}/>
+            </Route>
+
+
             <Route path="*" element={<Navigate to="/"/>}/>
+
         </Routes>
     )
 }
