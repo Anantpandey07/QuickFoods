b2f8dcd73d2a041ba2eeb23fada917cec4651656
Anantpandey07 <anantpandey175@gmail.com>
2024-08-10 11:44:58 +0530

    Toast Notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import Auth0providerwithNavigate from './auth/Auth0providerwithNavigate'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
       <Auth0providerwithNavigate>
       <AppRoutes/>
+      <Toaster visibleToasts={1} position="top-right" richColors/>
       </Auth0providerwithNavigate>
       </QueryClientProvider>
     </Router>
